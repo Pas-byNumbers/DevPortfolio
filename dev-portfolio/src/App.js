0d80e27bb7b7d9419e510106c09b1ffb6efec181
Pascal Rassaby <pasbynumbers@gmail.com>
2020-03-25 19:24:55 +0000

    switch package system to npm, grommet works

--- dev-portfolio/src/App.js
+++ dev-portfolio/src/App.js
@@ -1,25 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Grommet } from 'grommet'
+import './styles/App.css';
+import { Flag } from 'grommet-icons';
+
+const theme = {
+  global: {
+    font: {
+      family: 'Roboto',
+      size: '18px',
+      height: '20px',
+    },
+  },
+}
 
 function App() {
   return (
-    <div className="App">
+    <Grommet theme={theme} >
+      <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       
       </header>
     </div>
+    
+    </Grommet>
+    
   );
 }
 
